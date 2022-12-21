8923ad939ecc60ca30bf7d0c85e454f011a2e092
John Z Castillo <johnzunigacastillo@gmail.com>
2022-12-22 02:16:00 +0800

    final layout completed

--- views/sample.php
+++ views/sample.php
@@ -17,6 +17,37 @@ use views\components\Modules;
 </head>
 
 <body>
+    <style>
+        .content-right {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            border-radius: 0;
+            background-color: transparent !important;
+        }
+
+        .bio {
+            flex-basis: 20%;
+            border-radius: 10px;
+        }
+
+        .rainbow {
+            flex-basis: 80%;
+            padding: 6px;
+            border-radius: 10px;
+        }
+
+        .action {
+            display: flex;
+            padding-block: 20px;
+            flex-wrap: wrap;
+            gap: 20px;
+            height: 100%;
+            justify-content: space-evenly;
+            overflow-y: auto;
+            border-radius: 10px;
+        }
+    </style>
     <section class="main-wrapper bg bg-dashboard">
         <header class="header">COMPUTER AIDED INSTRUCTION MATERIAL FOR DATA STRUCTURE AND ALGORITHM</header>
         <section class="content">
@@ -29,20 +60,26 @@ use views\components\Modules;
                     ?>
                 </nav>
             </div>
-            <div class="content-right rainbow">
-                <div class="bio">
+            <div class="content-right">
+                <div class="bio bg bg-student">
                     <?php
                     //show default profile
-                    if (!isset($_SESSION['userProfile'])) {
-                        echo "<img src='./assets/profile/default.png' class='profile'>";
-                        exit();
-                    }
-                    echo "<img src='./assets/profile/" . $_SESSION['userProfile'] . "'" . " class='profile'>";
+                    // if (!isset($_SESSION['userProfile'])) {
+                    //     echo "<img src='./assets/profile/default.png' class='profile'>";
+                    //     exit();
+                    // }
+                    // echo "<img src='./assets/profile/" . $_SESSION['userProfile'] . "'" . " class='profile'>";
                     ?>
                 </div>
-                <div class="action">
+                <section class="rainbow bg bg-student">
+                    <div class="action">
+                        <a href="./my-score" class="btn-img bg bg-rewards scale shadow"></a>
+                        <a href="./my-score" class="btn-img bg bg-mastery scale shadow"></a>
+                        <a href="./my-score" class="btn-img bg bg-quiz-score scale shadow"></a>
+                        <a href="./my-score" class="btn-img bg bg-stats scale shadow"></a>
+                    </div>
+                </section>
 
-                </div>
             </div>
         </section>
     </section>
